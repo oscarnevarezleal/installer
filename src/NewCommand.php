4508d57c1d9c76eac6c1db8eba18115bf6c015c1
Oscar Nevarez <fu.wire@gmail.com>
2021-02-23 21:09:03 -0600

    debug

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -87,13 +87,11 @@ class NewCommand extends Command
 
         $commands = [];
 
-        $skipdownload = true;
-        $output->write('> skip-download?: ');
-        print_r($input->getOption('skip-download'), true);
+        $skipdownload = $input->getOption('skip-download');
 
         if ($skipdownload) {
-            $output->write(' ===> Skipping composer create-project: ');
-            $commands[] = 'ls -ltah ' . $directory;
+            $commands[] = "echo 'skipping composer create-project' ";
+            $commands[] = $this->getPhpWithArguments().' -m';
         } else {
             $create_project_cmd = $composer . " create-project laravel/laravel \"$directory\" $version --remove-vcs --prefer-dist";
             $output->write(' ===> Create project from command: ' . $create_project_cmd);
